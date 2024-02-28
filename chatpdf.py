b7b2d42e043e3b60b6ec8c74350ebf723ce42e8c
sannjai <143020985+sannjai@users.noreply.github.com>
2024-02-28 15:54:54 +0530

    Update chatpdf.py

--- chatpdf.py
+++ chatpdf.py
@@ -22,16 +22,6 @@ def get_pdf_text(pdf_docs):
     return  text
 
 
-
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
-
-
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     chunks = text_splitter.split_text(text)
